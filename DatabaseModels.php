e5b0d84524be88c313d6fcccc10c4026b3c7d590
MRsams <samuele.abba@gmail.com>
2016-03-22 01:39:46 +0100

    implemented some new route and controllers

--- DatabaseModels.php
+++ DatabaseModels.php
@@ -21,16 +21,16 @@ class Controller{
         $this->model = \Model::factory(str_replace('Controller', 'Model', get_class($this)));
     }
 
-    public function find_one($id=null, $resolved=false){
-        if(false === $resolved)
-            return $this->model->find_one($id);
-        return $this->model->find_one($id)->as_array();
+    public function find_one($id=null, $resolved=true){
+        if(true === $resolved)
+            return $this->model->find_one($id)->as_array();
+        return $this->model->find_one($id);
     }
 
-    public function find_many($resolved){
-        if(false === $resolved)
-            return $this->model->find_many();
-        return $this->model->find_array();
+    public function find_many($resolved=true){
+        if(true === $resolved)
+            return$this->model->find_array();
+        return $this->model->find_many();
     }
     
     public static function factory($className){
@@ -42,9 +42,36 @@ class Controller{
 
 }
 
+class Structure_price extends \Model{
+    public static $_table = 'structure_prices';
+}
+
+class Umbrella extends \Model{}
+
 class StructureController extends Controller{
 
+    public function getBeaches($resolved=true){
+        $beaches = $this->model->filter('type', 'beaches');
+        if(true === $resolved)
+            return $beaches->find_array();
+        return $beaches->find_many();
+    }
 
+    public function getPools($resolved=true){
+        $pools = $this->model->filter('type', 'pools');
+        if(true === $resolved)
+            return $pools->find_array();
+        return $pools->find_many();
+    }
+
+    public function getById($id){
+        $structure = $this->find_one($id, false);
+        $prices = $structure->has_one('structure_price')->find_one();
+
+        return $structure->as_array() + [
+            'prices' => $prices->as_array(),
+        ];
+    }
 
 }
 
@@ -62,6 +89,10 @@ class StructureModel extends \Model{
             return $orm->where('type', self::TYPE_POOL);
         else
             throw new InvalidArgumentException("type must be 'beaches' or 'pools'.");
+    }
+
+    public function price(){
+        return $this->has_one('structure_price');
     }
 
 }
--- index.php
+++ index.php
@@ -24,25 +24,25 @@ $app->group("/structure",
     function(){
         $this->get("",
             function(RequestInterface $request, ResponseInterface $response){
-                $response->getBody()->write(json_encode(Controller::factory('Structure')->find_many(true)));
+                $response->getBody()->write(json_encode(Controller::factory('Structure')->find_many()));
             });
 
-        $this->group("/beaches",
-            function(){
-                $this->get("",
-                    function(RequestInterface $request, ResponseInterface $response){
-                        $model = \Model::factory('StructureModel');
-                        $response->getBody()->write(json_encode($model->filter('type','beaches')->find_array()));
-                    });
+        $this->get("/beaches",
+            function(RequestInterface $request, ResponseInterface $response){
+                $beaches = Controller::factory('Structure')->getBeaches();
+                $response->getBody()->write(json_encode($beaches));
             });
 
-        $this->group("/pools",
-            function(){
-                $this->get("",
-                    function(RequestInterface $request, ResponseInterface $response){
-                        $model = \Model::factory('Structure');
-                        $response->getBody()->write(json_encode($model->filter('type','pools')->find_array()));
-                    });
+        $this->get("/pools",
+            function(RequestInterface $request, ResponseInterface $response){
+                $pools = Controller::factory('Structure')->getPools();
+                $response->getBody()->write(json_encode($pools));
+            });
+
+        $this->get("/{id:[0-9]+}",
+            function(RequestInterface $request, ResponseInterface $response, array $args){
+                $structure = Controller::factory('Structure');
+                $response->getBody()->write(json_encode($structure->getById($args['id'])));
             });
     });
 
